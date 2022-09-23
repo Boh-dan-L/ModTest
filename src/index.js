278d50a29e9ff78c3f6f4876c624d05c9b046084
Bogdan <bogdan@MacBook-Air-Bogdan.local>
2022-09-23 03:40:30 +0300

    Finish

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import './styles/index.scss'
 import './just-validate.min.js'
-import $ from "jquery";
-import { validate } from 'schema-utils';
-// import 'bootstrap';
 
+
+// Validate form
 
 new window.JustValidate('.js-form', {
-    rulse: {
-
-    },
+    rulse: {},
 
     colorWrong: '#D73F3E',
 
@@ -19,3 +16,18 @@ new window.JustValidate('.js-form', {
     }
 
 })
+
+// API endpoint, exchange rate
+
+    fetch(' https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5')
+.then (function(resp) {return resp.json()})
+.then (function(data){
+    console.log(data)
+    document.querySelector('.rate__dollar--text').textContent = data[0]['ccy'];
+    document.querySelector('.rate__dollar--buy').textContent = Math.round(data[0]['buy']*100)/100;
+    document.querySelector('.rate__dollar--sale').textContent = Math.round(data[0]['sale']*100)/100;
+    document.querySelector('.rate__eur--text').textContent = data[1]['ccy'];
+    document.querySelector('.rate__eur--buy').textContent = Math.round(data[1]['buy']*100)/100;
+    document.querySelector('.rate__eur--sale').textContent = Math.round(data[1]['sale']*100)/100;
+});
+
